520e7b806c159e6f89a4c27d42a75421a0a0b621
Erik Papikian <knara229@mail.ru>
2025-08-12 17:28:29 +0300

    task 2.2.1 is ready

---
+++ src/main/java/hiber/service/CarService.java
@@ -0,0 +1,13 @@
+package hiber.service;
+
+
+import hiber.model.Car;
+
+
+import java.util.List;
+
+public interface CarService {
+    public List<Car> listCars();
+
+    public void add(Car car);
+}
